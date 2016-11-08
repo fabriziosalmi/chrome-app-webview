bebaa8016cb6134073eb745eda052b40c4a3a304
fab <fabrizio.salmi@gmail.com>
2016-11-08 21:43:35 +0100

    Update rce.js

--- rce.js
+++ rce.js
@@ -1,7 +1,7 @@
 onload = function() {
   var element = document.createElement('webview');
   att = document.createAttribute('src');
-  att.value='http://www.google.com';
+  att.value='https://audiobooks.kisstube.tv';
   element.setAttributeNode(att);
   document.getElementById('rce-container').appendChild(element);
 }
